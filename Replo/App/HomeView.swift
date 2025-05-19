4e3e7a391d7dce6f34628baaf6692b009576ced2
lwer210 <lwer210@naver.com>
2025-05-19 20:27:37 +0900

    feat: [#2] SplashView 개발

---
+++ Replo/App/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Replo
+//
+//  Created by 이상훈 on 5/19/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
