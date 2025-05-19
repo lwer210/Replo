45bfc48e045dd298618e712b8b1825ff7a695530
lwer210 <lwer210@naver.com>
2025-05-19 19:13:58 +0900

    Initial Commit

---
+++ Replo/ReploApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReploApp.swift
+//  Replo
+//
+//  Created by 이상훈 on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ReploApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReploTests/ReploTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReploTests.swift
+//  ReploTests
+//
+//  Created by 이상훈 on 5/19/25.
+//
+
+import Testing
+@testable import Replo
+
+struct ReploTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
