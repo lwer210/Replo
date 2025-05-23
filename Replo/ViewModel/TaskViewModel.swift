19601d8a56b68edca038aeda325b201e6155096f
lwer210 <lwer210@naver.com>
2025-05-24 00:41:21 +0900

    feature: [#10] ViewModel에 정의된 tasks 배열 데이터 삽입 메서드 추가

--- Replo/ViewModel/TaskViewModel.swift
+++ Replo/ViewModel/TaskViewModel.swift
@@ -10,6 +10,19 @@ import SwiftUI
 
 // Task ViewModel 선언
 class TaskViewModel: ObservableObject{
-    // Task 배열 선언
+    
+    /// Task 배열 필드
     @Published var tasks: [Task] = []
+    
+    /// Task 검색어 필드
+    @Published var taskSearchText = ""
+    
+    /// Tasks 배열에 요소 추가
+    /// - Parameters text: Task에 추가할 텍스트 요소값
+    func addTask(text: String){
+        if !text.isEmpty{ // text가 빈값이 아닐 때만 추가
+            let task = Task(isComplete: false, todo: text)
+            tasks.append(task)
+        }
+    }
 }
