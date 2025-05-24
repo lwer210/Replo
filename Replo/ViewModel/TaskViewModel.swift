f38a8e9e9bf74bc764d43fc6b6221ec470231a75
lwer210 <lwer210@naver.com>
2025-05-24 10:51:08 +0900

    feat: [#11] Task 화면 Preview 테스트

--- Replo/ViewModel/TaskViewModel.swift
+++ Replo/ViewModel/TaskViewModel.swift
@@ -18,11 +18,42 @@ class TaskViewModel: ObservableObject{
     @Published var taskSearchText = ""
     
     /// Tasks 배열에 요소 추가
+    ///
     /// - Parameters text: Task에 추가할 텍스트 요소값
     func addTask(text: String){
         if !text.isEmpty{ // text가 빈값이 아닐 때만 추가
             let task = Task(isComplete: false, todo: text)
             tasks.append(task)
         }
+    }
+    
+    /// Task 완료 / 취소 처리 메서드
+    ///
+    /// - Parameters task: 완료 / 취소 처리할 Task
+    func toggleIsCompleted(task: Task){
+        let index = tasks.firstIndex(where: {
+            $0.id == task.id
+        })
+        
+        if let index{
+            tasks[index].isComplete.toggle()
+        }
+    }
+    
+    /// Task 완료 / 취소 여부 확인 메서드
+    ///
+    /// - Parameters task: 완료 / 취소 여부 확인이 필요한 task
+    /// - Returns: 완료 / 취소 여부
+    func checkIsCompleted(task: Task) -> Bool{
+        let index = tasks.firstIndex(where: {
+            $0.id == task.id
+        })
+        
+        if let index{
+            print("\(tasks[index].isComplete)")
+            return tasks[index].isComplete
+        }
+        
+        return false
     }
 }
