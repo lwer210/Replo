ef38f74cb3e9d6951d1f4ac683f896d5ce2d7597
lwer210 <lwer210@naver.com>
2025-05-25 10:18:28 +0900

    fix: [#15] Task 컴포넌트 디자인 정리

---
+++ Replo/View/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Replo
+//
+//  Created by 이상훈 on 5/25/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
